777197fe04d88aec15dc13b02adf226920ae2552
peter.lin@uha.fr <peter.lin@uha.fr@6219d9c7-8401-0010-ae78-9bc666ba87c6>
2022-12-05 14:42:48 +0000

    ajout de la fonction turn

--- main.c
+++ main.c
@@ -2,5 +2,24 @@
 #include "main.h"
 
 int main(int argc, char *argv[]) {
+	int i=1;
+	while(game!=0) {
+		if(turn(i)==2) {
+		printf("C'est au tour des noirs de jouer\n");
+		
+		
+		}
+		else {
+		printf("C'est au tour des blancs de jouer \n");
+		
+		
+		}
+		i++;
+	}
+	
+}
 
+int turn(int current_turn){
+	if (current_turn%2+1==2) return 2;
+	else return 1;
 }
