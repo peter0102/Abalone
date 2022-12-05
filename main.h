ed73a2eaf92cdd4e49bf4b01361f93b0f747dc12
isaac.dalberto@uha.fr <isaac.dalberto@uha.fr@6219d9c7-8401-0010-ae78-9bc666ba87c6>
2022-12-05 15:00:11 +0000

    Ajout des dictionnaires de coordonnées et de la fonction de traduction texte/index

--- main.h
+++ main.h
@@ -1,3 +1,8 @@
-#include <stdio.h>
 int turn(int current_turn);
 
+int coords_nbs_dictionary (char chr);
+int coords_ltr_dictionary (char chr);
+
+typedef struct {int x1; int y1; int x2; int y2;} Coords;
+Coords translate_coords(char* input);
+void print_coords(Coords coords);
