2a0b8e59cb89b42c235ce624e70493b3ed6b29bd
isaac.dalberto@uha.fr <isaac.dalberto@uha.fr@6219d9c7-8401-0010-ae78-9bc666ba87c6>
2022-12-06 09:19:52 +0000

    Suppression de l'include stdlib (car inutilisé) - Déplacement de la fonction main à la fin de main.c - Déplacement de la déclaration du type Tableau et de la fonction display dans main.h - Ajout de la documentation pour la traduction des coups dans main.h

--- main.h
+++ main.h
@@ -1,10 +1,49 @@
+/**
+ * \file main.h
+ * \brief Fonctions principales du jeu
+ * \author Groupe 1
+ * \version 0.1
+*/
+
 int turn(int current_turn);
 
-int coords_nbs_dictionary (char chr);
-int coords_ltr_dictionary (char chr);
+typedef int Tableau[MAX_I][MAX_J];
+void display ();
 
-typedef struct {int x1; int y1; int x2; int y2;} Coords;
+/**
+ * Dictionnaire numérique pour les mouvements
+*/
+int coords_nbs_dictionary (char chr);
+/**
+ * Dictionnaire alphabéthique pour les mouvements
+*/
+int coords_ltr_dictionary (char chr);
+/**
+ * \struct Coords main.h main.c
+ * \brief Binôme point de départ/point d'arrivée d'un mouvement (16 octets)
+*/
+typedef struct {
+    /*! Ligne du point de départ (lettre) */
+    int x1;
+    /*! Colonne du point de départ (chiffre) */
+    int y1;
+    /*! Ligne du point d'arrivée (lettre) */
+    int x2;
+    /*! Colonne du point d'arrivée (chiffre) */
+    int y2;} Coords;
+/**
+ * \fn translate_coords(char* input)
+ * \brief traduction
+ * \param input chaîne de caractères des coordonnées (ex: A1:B2)
+ * \return coordonnées numériques des points de départ et d'arrivée
+*/
 Coords translate_coords(char* input);
+/**
+ * \fn print_coords(Coords coords)
+ * \brief affichage des coordonnées
+ * \param coords coordonées numériques à afficher
+ * \return Nothing
+*/
 void print_coords(Coords coords);
 
 int isItWin();
