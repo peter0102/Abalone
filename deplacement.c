176412b458b980e5c0002d1f821b4029e9d210b6
isaac.dalberto <isaac.dalberto@uha.fr>
2022-12-08 17:55:04 +0000

    Modification de la bibliothèque de déplacements pour remplacer les struct Move par des int** - Ajustements mineurs dans la documentation.

--- deplacement.c
+++ deplacement.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "deplacement.h"
 
-int move_chr_convert (char chr, char type) {
+int moveChrConvert (char chr, char type) {
     int index = -1;
     switch (type) {
         case 'l': index = (int)(chr-'@'); break;
@@ -13,7 +13,7 @@ int move_chr_convert (char chr, char type) {
     return (index > 0 && index < 9) ? index : -1;
 }
 
-char move_chr_convert_reverse (int index, char type) {
+char moveChrConvertReverse (int index, char type) {
     if (index < 1 || index > 8) return 'x';
     switch (type) {
         case 'l': return (char)(index + 64); //64 = @
@@ -22,47 +22,52 @@ char move_chr_convert_reverse (int index, char type) {
     }
 }
 
-void move_change_all(Move* move, int new_coords[4]) {
-    move->x1 = new_coords[0];
-    move->y1 = new_coords[1];
-    move->x2 = new_coords[2];
-    move->y2 = new_coords[3];
-}
-
-Move translate_move(char* input) {
-    Move move = {-1, -1, -1, -1};
+int** translateMove(char* input) {
+    int** move = createMove(-1, -1, -1, -1);
 
     //Si la longueur de l'entree n'est pas 5, il y a erreur
     if (strlen(input) != 5) return move;
-
     //Si le 3eme caractere n'est pas ':', il y a erreur
     if (input[2] != ':') return move;
 
     int numbers[] = {
-        move_chr_convert(input[0], 'l'),
-        move_chr_convert(input[1], 'n'),
-        move_chr_convert(input[3], 'l'),
-        move_chr_convert(input[4], 'n')
+        moveChrConvert(input[0], 'l'),
+        moveChrConvert(input[1], 'n'),
+        moveChrConvert(input[3], 'l'),
+        moveChrConvert(input[4], 'n')
     };
-
     for (int i = 0; i < 4; i++) {
         if (numbers[i] == -1) return move;
     }
-    move_change_all(&move, numbers);
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++)
+            move[i][j] = numbers[(2*i)+j];
+    }
     return move;
 }
 
-char* translate_move_reverse(Move move) {
+char* translateMoveReverse(int** move) {
     char* str = malloc(5);
 
-    str[0] = move_chr_convert_reverse(move.x1, 'l');
-    str[1] = move_chr_convert_reverse(move.y1, 'n');
+    str[0] = moveChrConvertReverse(move[0][0], 'l');
+    str[1] = moveChrConvertReverse(move[0][1], 'n');
     str[2] = ':';
-    str[3] = move_chr_convert_reverse(move.x2, 'l');
-    str[4] = move_chr_convert_reverse(move.y2, 'n');
+    str[3] = moveChrConvertReverse(move[1][0], 'l');
+    str[4] = moveChrConvertReverse(move[1][1], 'n');
     return str;
 }
 
-void print_move(Move move) {
-    printf("%i, %i\n%i, %i\n\n", move.x1, move.y1, move.x2, move.y2);
+int** createMove(int x1, int y1, int x2, int y2) {
+    int** move = malloc(2 * sizeof(int*));
+    for(int i = 0; i < 2; i++)
+        move[i] = malloc(2 * sizeof(int));
+    move[0][0] = x1;
+    move[0][1] = y1;
+    move[1][0] = x2;
+    move[1][1] = y2;
+    return move;
+}
+
+void printMove(int** move) {
+    printf("%i, %i\n%i, %i\n\n", move[0][0], move[0][1], move[1][0], move[1][1]);
 }
