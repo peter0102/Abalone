929ae2af2d1cf1a58743575631c5ff240e1473c1
alexandre.caux <alexandre.caux@uha.fr>
2022-12-12 14:05:17 +0000

    Premières corrections du main

--- main.c
+++ main.c
@@ -19,7 +19,7 @@
 #define ERROR 'x'
 #define success 'l'
 //définition des types
-typedef char Plateau[10][10]; //caractères ASCII
+typedef char Board[10][10]; //caractères ASCII
 typedef char Move[2][2]; //Mouvement {{x0,x1},{y0,y1}} valeurs numériques
 typedef char Triple[2][3]; //coordonnées d'un joueur {{xj0,xj1,xj2},{yj0,yj1,yj2}} valeurs numériques
 
@@ -31,20 +31,9 @@ typedef char Triple[2][3]; //coordonnées d'un joueur {{xj0,xj1,xj2},{yj0,yj1,yj
 
 char* moves[200];
 
-Plateau plateau = {
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, EMPTY},
-    {EMPTY, EMPTY, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-    {EMPTY, EMPTY, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, EMPTY, EMPTY},
-    {EMPTY, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, EMPTY},
-    {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}
-};
 
-void display(Plateau p){
+
+void display(Board board){
     for (int i=1; i<MAX_I-1; i++){
         for (int j=1; j<MAX_J-1; j++){
             if (p[i][j]==WHITE){
@@ -61,133 +50,37 @@ void display(Plateau p){
     }
 }
 
-int isItWin(Plateau p){
-    int victory = 0;
-        for (int i=0; i<=8; i++){
-            if (p[0][i]!=0) {victory = 1;}
-            if (p[i][9]!=0) {victory = 1;}
-            if (p[9][(9-i)]!=0) {victory = 1;}
-            if (p[(9-i)][0]!=0) {victory = 1;}
+char isItWin(Board board){
+    for(char i=0; i<=8; i++){
+        if(p[0][i]!=0 || p[i][9]!=0 || p[9][(9-i)]!=0 || p[(9-i)][0]!=0){
+            return(ERROR);
         }
-    if (victory==1){
-        printf("The game is won !\n\n");
     }
-    return victory;
-}
-
-int miniMax(Plateau p,int depth,int alpha,int beta,bool isMaximizingPlayer){
-	int lengthOfMoves=sizeof(moves)/sizeof(moves[0]); // taille de la liste de mouvements
-    alpha=-INFTY;
-    beta=INFTY;
-    Move m;
-    Move mback;
-	if(depth==0) //S'arrête lorsque la profondeur souhaitée est atteinte
-		return evaluate(p,BLACK);
-	    
-	if(isMaximizingPlayer){ 
-        int score=-INFTY;
-        for (int i=0;i<lengthOfMoves;i++) {
-            char* charac=moves[i];
-            translateMove(charac);
-			m[0][0]=charac[0]-'@';
-        	m[1][0]=charac[1]-'0';
-        	m[0][1]=charac[3]-'@';
-        	m[1][1]=charac[4]-'0';
-            allMove(p,m,BLACK,WHITE); // effectue un louvement
-			int newScore=miniMax(p,depth-1,alpha,beta,false); // simule le tour de l'adversaire
-        	mback[0][0]=m[0][1];
-        	mback[1][0]=m[1][1];
-        	mback[0][1]=m[0][0];
-        	mback[1][1]=m[1][0];
-            allMove(p,mback,BLACK,WHITE); // on annule le mouvement joué
-			if (newScore>score) score=newScore; // on prend le meilleur score
-			if (alpha>newScore) alpha=newScore;
-            if (alpha>=beta) break; // elagage
-        }
-		return score;
-	}
-	else {
-        int score=INFTY;
-        for (int i=0;i<lengthOfMoves;i++) {
-            char* charac=moves[i];
-            translateMove(charac);
-			m[0][0]=charac[0]-'@';
-        	m[1][0]=charac[1]-'0';
-        	m[0][1]=charac[3]-'@';
-        	m[1][1]=charac[4]-'0';
-            allMove(p,m,WHITE,BLACK);
-			int newScore=miniMax(p,depth-1,alpha,beta,true); // simule le tour de l'adversaire
-        	mback[0][0]=m[0][1];
-        	mback[1][0]=m[1][1];
-        	mback[0][1]=m[0][0];
-        	mback[1][1]=m[1][0];
-            allMove(p,mback,WHITE, BLACK);
-			if (newScore<score) score=newScore; // on prend le moins bon score
-			if (beta<newScore) alpha=newScore;
-            if (beta<=alpha) break; // elagage
-        }
-		return score;
-	}
-}
-
-
-char aiMove(Plateau p){
-    int lengthOfMoves=sizeof(moves)/sizeof(moves[0]); // taille de la liste de mouvements
-    Move m;
-    Move mback; // pour "annuler" un mouvement
-    Move bestMove; // prend le meilleur mouvement
-    int score=-INFTY;
-    for(int i=0;i<lengthOfMoves;i++){
-        char* charac=moves[i];
-        translateMove(charac);
-        m[0][0]=charac[0]-'@';
-        m[1][0]=charac[1]-'0';
-        m[0][1]=charac[3]-'@';
-        m[1][1]=charac[4]-'0';
-        allMove(p,m,BLACK,WHITE);
-        int newScore=miniMax(p,0,-INFTY,INFTY,false);
-        mback[0][0]=m[0][1];
-        mback[1][0]=m[1][1];
-        mback[0][1]=m[0][0];
-        mback[1][1]=m[1][0];
-        allMove(p,mback,BLACK,WHITE);
-            if (newScore>score) {
-            score=newScore;
-            bestMove[0][0]=m[0][0];
-            bestMove[0][1]=m[0][1];
-            bestMove[1][0]=m[1][0];
-            bestMove[1][1]=m[1][1];
-            }
-        }
-    allMove(p,bestMove,BLACK,WHITE);
-}
-
-
-char playerMove(Plateau p){
-    Move m;
-    char charac[1000]="";
-    printf("Quel mouvement ? de type xx:xx\n");
-    scanf("%s",charac);
-    translateMove(charac);
-    m[0][0]=charac[0]-'@';
-    m[1][0]=charac[1]-'0';
-    m[0][1]=charac[3]-'@';
-    m[1][1]=charac[4]-'0';
-    char a=allMove(p,m,WHITE,BLACK);
-    printf("\n\nRetour de la fonction allMove : %d",a);
+    return SUCCESS;
 }
 
 int main(int argc, char *argv[]) {
+    Board b = {
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, EMPTY},
+            {EMPTY, EMPTY, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, EMPTY, EMPTY},
+            {EMPTY, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}
+    };
     char current_player;
     char other_player;
-    int win_condition=isItWin(plateau);
-    int turn_count=1;
+    int turn_count;
     printf("Début du jeu\n");
     display(plateau);
-    while (win_condition !=1) {
+    while (isItWin(p)!=ERROR){
 		if(turn_count%2==1) { //turn_count = 1 --> current_player = 'B'
-            current_player = 'B';
-            other_player = 'W';
+            current_player = BLACK;
+            other_player = WHITE;
             printf("Tour %i\n",turn_count);
 		    printf("C'est au tour des noirs (IA) de jouer\n");
 		    aiMove(plateau);
@@ -195,8 +88,8 @@ int main(int argc, char *argv[]) {
             printf("\n*************************************\n");
 		}
 		else { //turn_count = 2 --> current_player = 'W'
-            current_player = 'W';
-            other_player = 'B';
+            current_player = WHITE;
+            other_player = BLACK;
             printf("Tour %i\n",turn_count);
 	    	printf("C'est à votre tour de jouer\n");
 		    playerMove(plateau);
