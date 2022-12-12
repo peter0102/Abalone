7bab46c35da248e98586834bf8a33bfb1199fffd
isaac.dalberto@uha.fr <isaac.dalberto@uha.fr@6219d9c7-8401-0010-ae78-9bc666ba87c6>
2022-12-12 15:16:33 +0000

    Ajout d'un squelette pour la réalisation de tests - Ajout d'un début de fichier de tests pour move.c

---
+++ tests/move.test.c
@@ -0,0 +1,16 @@
+//#include "../move.h"
+#include <stdio.h>
+
+
+
+void runTest(int (*f)(), int* failures, int* runs) {
+    if (!f()) (*failures)++;
+    (*runs)++;
+}
+
+int main(int argc, char* argv[]) {
+    int test_count = 0, failures = 0;
+    //Insérer les runTest(nom_fonction, &failures, &runs);
+    printf("%d failures out of %d tests\n", failures, test_count);
+    return failures;
+}
