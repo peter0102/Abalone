67bfa646a1be7bda7e493f6b3038cf13b2371a25
alexandre.caux <alexandre.caux@uha.fr>
2022-12-12 10:46:46 +0000

    Remplacement des vieux noms de variables par les nouveaux

--- allMove.c
+++ allMove.c
@@ -1,21 +1,21 @@
 //joueurs
 //#define current_player 'W'
 //#define other_player 'B'
-#define no_player '0'
+#define EMPTY '0'
 //types de mouvement
-#define line_horizontal_right 'a'
-#define line_horizontal_left 'b'
-#define line_vertical_down 'c'
-#define line_vertical_up 'd'
-#define lateral_two_horizontal 'e'
-#define lateral_two_vertical 'f'
-#define lateral_three_vertical_x0_down 'g'
-#define lateral_three_vertical_x0_up 'h'
-#define lateral_three_horizontal_y0_right 'i'
-#define lateral_three_horizontal_y0_left 'j'
+#define LINE_HOR_RIGHT 'a'
+#define LINE_HOR_LEFT 'b'
+#define LINE_VER_DOWN 'c'
+#define LINE_VER_UP 'd'
+#define LAT_2_HOR 'e'
+#define LAT_2_VER 'f'
+#define LAT_3_VER_X0_DOWN 'g'
+#define LAT_3_VER_X0_UP 'h'
+#define LAT_3_HOR_Y0_RIGHT 'i'
+#define LAT_3_HOR_Y0_LEFT 'j'
 //gestion d'erreurs
-#define error 'x'
-#define success 'l'
+#define ERROR 'x'
+#define SUCCESS 'l'
 //définition des types
 typedef char Plateau[10][10];
 typedef char Move[2][2];
@@ -31,80 +31,87 @@ char absChar(char v){
     }
 }
 
+char checkSuicide(Move m){
+    if(m[0][1]==0 || m[0][1]==9 || m[1][1]==0 || m[1][1]==9){
+        return(ERROR);
+    }
+    return(SUCCESS);
+}
+
 char whatMove(Plateau plat,Move m, char current_player) {
     //déplacement en ligne horizontal
     if (m[0][0] == m[0][1]) {
         //déplacement vers la droite
         if (m[1][0] < m[1][1]) {
-            return (line_horizontal_right);
+            return (LINE_HOR_RIGHT);
         }
         //déplacement vers la gauche
         if (m[1][0] > m[1][1]) {
-            return (line_horizontal_left);
+            return (LINE_HOR_LEFT);
         }
     }
         //déplacement en ligne vertical
     else if (m[1][0] == m[1][1]) {
         //déplacement vers le bas
         if (m[0][0] < m[0][1]) {
-            return (line_vertical_down);
+            return (LINE_VER_DOWN);
         }
         //déplacement vers le haut
         if (m[0][0] > m[0][1]) {
-            return (line_vertical_up);
+            return (LINE_VER_UP);
         }
     }
         //déplacement latéral de taille 2
     else if ((absChar(m[0][0] - m[0][1]) + absChar(m[1][0] - m[1][1])) == 2) {
         //déplacement latéral horizontal
         if (plat[m[0][0]][m[1][1]] == current_player) {
-            return (lateral_two_horizontal);
+            return (LAT_2_HOR);
         }
             //déplacement latéral vertical
         else if (plat[m[0][1]][m[1][0]] == current_player) {
-            return (lateral_two_vertical);
+            return (LAT_2_VER);
         }
     }
         //déplacement latéral vertical de taille 3
     else if (absChar(m[0][0] - m[0][1]) == 2 && absChar(m[1][0] - m[1][1]) == 1) {
         //x0 bas & x1 haut
         if (m[0][0] > m[0][1]) {
-            return (lateral_three_vertical_x0_down);
+            return (LAT_3_VER_X0_DOWN);
         }
         //x0 haut & x1 bas
         if (m[0][0] < m[0][1]) {
-            return (lateral_three_vertical_x0_up);
+            return (LAT_3_VER_X0_UP);
         }
     }
         //déplacement latéral horizontal de taille 3
     else if (absChar(m[0][0] - m[0][1]) == 1 && absChar(m[1][0] - m[1][1]) == 2) {
         //y0 droite & y1 gauche
         if (m[1][0] < m[1][1]) {
-            return (lateral_three_horizontal_y0_right);
+            return (LAT_3_HOR_Y0_RIGHT);
         }
         //y0 gauche & y1 droite
         if (m[1][0] > m[1][1]) {
-            return (lateral_three_horizontal_y0_left);
+            return (LAT_3_HOR_Y0_LEFT);
         }
     }
-    return (error);
+    return (ERROR);
 }
 
 char allies(char type_of_move,Move m){
-    if(type_of_move==line_horizontal_right || type_of_move==line_horizontal_left){
+    if(type_of_move==LINE_HOR_RIGHT || type_of_move==LINE_HOR_LEFT){
         return(absChar(m[1][0] - m[1][1]));
     }
-    else if(type_of_move==line_vertical_up || type_of_move==line_vertical_down) {
+    else if(type_of_move==LINE_VER_UP || type_of_move==LINE_VER_DOWN) {
         return(absChar(m[0][0] - m[0][1]));
     }
-    else if(type_of_move==lateral_two_horizontal || type_of_move==lateral_two_vertical) {
+    else if(type_of_move==LAT_2_HOR || type_of_move==LAT_2_VER) {
         return(2);
     }
-    else if(type_of_move==lateral_three_vertical_x0_down || type_of_move==lateral_three_vertical_x0_up ||\
-    type_of_move==lateral_three_horizontal_y0_right || type_of_move==lateral_three_horizontal_y0_left){
+    else if(type_of_move==LAT_3_VER_X0_DOWN || type_of_move==LAT_3_VER_X0_UP ||\
+    type_of_move==LAT_3_HOR_Y0_RIGHT || type_of_move==LAT_3_HOR_Y0_LEFT){
         return(3);
     }
-    return(error);
+    return(ERROR);
 }
 
 void active(Move m,char type_of_move,char nb_allies,Triple coords_allies,Triple coords_ennemies){
@@ -117,70 +124,70 @@ void active(Move m,char type_of_move,char nb_allies,Triple coords_allies,Triple 
     //disjonctions de cas
     for(char i = 1;i<nb_allies;i++) {
         switch (type_of_move) {
-            case line_horizontal_right: {
+            case LINE_HOR_RIGHT: {
                 coords_allies[0][i] = m[0][0];
                 coords_allies[1][i] = m[1][0] + i;
                 coords_ennemies[0][i] = m[0][1];
                 coords_ennemies[1][i] = m[1][1] + i;
             }
                 break;
-            case line_horizontal_left: {
+            case LINE_HOR_LEFT: {
                 coords_allies[0][i] = m[0][0];
                 coords_allies[1][i] = m[1][0] - i;
                 coords_ennemies[0][i] = m[0][1];
                 coords_ennemies[1][i] = m[1][1] - i;
             }
                 break;
-            case line_vertical_down: {
+            case LINE_VER_DOWN: {
                 coords_allies[0][i] = m[0][0] + i;
                 coords_allies[1][i] = m[1][0];
                 coords_ennemies[0][i] = m[0][1] + i;
                 coords_ennemies[1][i] = m[1][1];
             }
                 break;
-            case line_vertical_up: {
+            case LINE_VER_UP: {
                 coords_allies[0][i] = m[0][0] - i;
                 coords_allies[1][i] = m[1][0] ;
                 coords_ennemies[0][i] = m[0][1] - i;
                 coords_ennemies[1][i] = m[1][1];
             }
                 break;
-            case lateral_two_horizontal: {
+            case LAT_2_HOR: {
                 coords_allies[0][1] = m[0][0];
                 coords_allies[1][1] = m[1][1];
                 coords_ennemies[0][1] = m[0][1];
                 coords_ennemies[1][1] = m[1][0];
             }
                 break;
-            case lateral_two_vertical: {
+            case LAT_2_VER: {
                 coords_allies[0][1] = m[0][1];
                 coords_allies[1][1] = m[1][0];
                 coords_ennemies[0][1] = m[0][0];
                 coords_ennemies[1][1] = m[1][1];
             }
                 break;
-            case lateral_three_vertical_x0_down: {
+            case LAT_3_VER_X0_DOWN: {
                 coords_allies[0][i] = m[0][0] - i;
                 coords_allies[1][i] = m[1][0];
                 coords_ennemies[0][i] = m[0][1] + i;
                 coords_ennemies[1][i] = m[1][1];
             }
                 break;
-            case lateral_three_vertical_x0_up: {
+            case LAT_3_VER_X0_UP: {
                 coords_allies[0][i] = m[0][0] + i;
                 coords_allies[1][i] = m[1][0];
                 coords_ennemies[0][i] = m[0][1] - i;
                 coords_ennemies[1][i] = m[1][1];
             }
                 break;
-            case lateral_three_horizontal_y0_right:{
+            case LAT_3_HOR_Y0_RIGHT:{
                 coords_allies[0][i] = m[0][0];
                 coords_allies[1][i] = m[1][0] + i;
                 coords_ennemies[0][i] = m[0][1];
                 coords_ennemies[1][i] = m[1][1] - i;
             }
                 break;
-            case lateral_three_horizontal_y0_left:{
+            case LAT_3_HOR_Y0_LEFT:{
                 coords_allies[0][i] = m[0][0];
                 coords_allies[1][i] = m[1][0] - i;
                 coords_ennemies[0][i] = m[0][1];
@@ -194,28 +201,31 @@ char ennemies(Plateau plat,Triple coords_ennemies,char nb_allies){
     char nb_ennemies=0;
     for(char i=0;i<nb_allies;i++) {
         if(plat[coords_ennemies[0][i]][coords_ennemies[1][i]]==current_player){
-            return(error);
+            return(ERROR);
         }
-        else if(plat[coords_ennemies[0][i]][coords_ennemies[1][i]]==no_player){
+        else if(plat[coords_ennemies[0][i]][coords_ennemies[1][i]]==EMPTY){
             return(nb_ennemies);
         }
         else{
             nb_ennemies++;
         }
     }
-    return(error);
+    return(ERROR);
 }
 
 char checkColor(Triple coords,char color, char nb_allies, Plateau plat){
     for(char i=0;i<nb_allies;i++) {
         if(plat[coords[0][i]][coords[1][i]]!=color){
-            return(error);
+            return(ERROR);
         }
     }
-    return(success);
+    return(SUCCESS);
 }
 
 char checkMove(Plateau plat, Move m,char current_player) {
+    if(checkSuicide(m)==ERROR){
+        return(ERROR);
+    }
     char type_of_move = whatMove(plat,m,current_player);
     char nb_allies = allies(type_of_move,m);
     //Initialisation des actifs (alliés + ennemis)
@@ -223,31 +233,31 @@ char checkMove(Plateau plat, Move m,char current_player) {
     Triple coords_ennemies;
     active(m, type_of_move, nb_allies, coords_allies, coords_ennemies);
     char nb_ennemies = ennemies(plat, coords_ennemies, nb_allies);
-    if(nb_ennemies == error){
-        return(error);
+    if(nb_ennemies == ERROR){
+        return(ERROR);
     }
     //vérification des couleurs
-    if(checkColor(coords_allies,current_player,nb_allies,plat)==error){
-        return(error);
+    if(checkColor(coords_allies,current_player,nb_allies,plat)==ERROR){
+        return(ERROR);
     }
-    return(success);
+    return(SUCCESS);
 }
 
 char pierre(Plateau plat, Move m, char current_player){
-    if(checkMove(plat,m,current_player)==error){
+    if(checkMove(plat,m,current_player)==ERROR){
         if(plat[m[1][0]][m[1][1]]==current_player){
             return(current_player);
         }
         else{
-            return(error);
+            return(ERROR);
         }
     }
-    return(success);
+    return(SUCCESS);
 }
 
 void moveLine(Plateau plat,Move m,Triple coords_ennemies,char nb_ennemies,char current_player,char other_player){
     //déplacement des alliés
-    plat[m[0][0]][m[1][0]] = no_player;
+    plat[m[0][0]][m[1][0]] = EMPTY;
     plat[coords_ennemies[0][0]][coords_ennemies[1][0]] = current_player;
     //déplacement des ennemis
     if (nb_ennemies > 0) {
@@ -257,7 +267,7 @@ void moveLine(Plateau plat,Move m,Triple coords_ennemies,char nb_ennemies,char c
 
 void moveLateral(Plateau plat,Triple coords_allies,Triple coords_ennemies, char nb_allies){
     for(char i=0;i<nb_allies;i++){
-        plat[coords_allies[0][i]][coords_allies[1][i]]=no_player;
+        plat[coords_allies[0][i]][coords_allies[1][i]]=EMPTY;
         plat[coords_ennemies[0][i]][coords_ennemies[1][i]]=current_player;
     }
 }
@@ -265,8 +275,8 @@ void moveLateral(Plateau plat,Triple coords_allies,Triple coords_ennemies, char 
 void move(Plateau plat,Move m,Triple coords_ennemies,char nb_ennemies,char type_of_move,Triple coords_allies,\
 char nb_allies,char current_player, char other_player){
     //déplacement en ligne
-    if(type_of_move == line_vertical_down || type_of_move == line_vertical_up ||\
-    type_of_move== line_horizontal_left || type_of_move == line_horizontal_right) {
+    if(type_of_move == LINE_VER_DOWN || type_of_move == LINE_VER_UP ||\
+    type_of_move== LINE_HOR_LEFT || type_of_move == LINE_HOR_RIGHT) {
         moveLine(plat,m,coords_ennemies,nb_ennemies,current_player,other_player);
     }
         //déplacement latéral
@@ -275,24 +285,23 @@ char nb_allies,char current_player, char other_player){
     }
 }
 
-char checkBorders(Move m){
-
-}
-
 char allMove(Plateau plat,Move m,char current_player,char other_player){
+    if(checkSuicide(m)==ERROR){
+        return(ERROR);
+    }
     char type_of_move = whatMove(plat,m,current_player);
     char nb_allies = allies(type_of_move,m);
-    if(nb_allies == error){ //si le type de mouvement est error nb_allies renvoie error
-        return(error);
+    if(nb_allies == ERROR){ //si le type de mouvement est ERROR nb_allies renvoie ERROR
+        return(ERROR);
     }
     Triple coords_allies;
     Triple coords_ennemies;
     active(m, type_of_move, nb_allies, coords_allies, coords_ennemies);
     char nb_ennemies = ennemies(plat,coords_ennemies,nb_allies);
     char good_color = checkColor(coords_allies,current_player,nb_allies,plat);
-    if(nb_ennemies == error || good_color == error){
-        return(error);
+    if(nb_ennemies == ERROR || good_color == ERROR){
+        return(ERROR);
     }
     move(plat,m,coords_ennemies,nb_ennemies,type_of_move,coords_allies,nb_allies,current_player,other_player);
-    return(success);
+    return(SUCCESS);
 }
