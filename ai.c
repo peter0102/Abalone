944958e57a8fc88f572e064757de7fff721926fb
peter.lin <peter.lin@uha.fr>
2022-12-12 18:21:20 +0000

    Modification de miniMax et aiMove

--- ai.c
+++ ai.c
@@ -13,7 +13,6 @@ int miniMax(Plateau p,int depth,int alpha,int beta,bool isMaximizingPlayer){
     beta=INFTY;
     Move m;
     Move mback;
-    Move bestMove;
 	if(depth==0){ //S'arrête lorsque la profondeur souhaitée est atteinte
 		return evaluate(p,BLACK);
 	}
@@ -22,10 +21,13 @@ int miniMax(Plateau p,int depth,int alpha,int beta,bool isMaximizingPlayer){
         int score=-INFTY;
         for (int i=0;i<lengthOfMoves;i++) {
             char* charac=moves[i];
-            m=translateMove(charac);
+            translateMove(m,charac);
             allMove(p,m,BLACK,WHITE); // effectue un mouvement
 			int newScore=miniMax(p,depth-1,alpha,beta,false); // simule le tour de l'adversaire
-            Move mback=createMove(m[0][1],m[1][1],m[0][0],m[1][0]);
+            mback[0][0]=m[0][1];
+            mback[1][0]=m[1][1];
+            mback[0][1]=m[0][0];
+            mback[1][1]=m[1][0];
             allMove(p,mback,BLACK,WHITE); // on annule le mouvement joué
 			if (newScore>score) score=newScore; // on choisit le mouvement donnant le meilleur score
 			if (alpha>newScore) alpha=newScore;
@@ -38,10 +40,13 @@ int miniMax(Plateau p,int depth,int alpha,int beta,bool isMaximizingPlayer){
         int score=INFTY;
         for (int i=0;i<lengthOfMoves;i++) {
             char* charac=moves[i];
-            m=translateMove(charac);
+            translateMove(m,charac);
             allMove(p,m,WHITE,BLACK);
 			int newScore=miniMax(p,depth-1,alpha,beta,true); // simule le tour de l'adversaire
-            Move mback=createMove(m[0][1],m[1][1],m[0][0],m[1][0]);
+            mback[0][0]=m[0][1];
+            mback[1][0]=m[1][1];
+            mback[0][1]=m[0][0];
+            mback[1][1]=m[1][0];
             allMove(p,mback,WHITE, BLACK);
 			if (newScore<score) score=newScore; // on prend le moins bon score
 			if (beta<newScore) alpha=newScore;
@@ -60,15 +65,21 @@ char aiMove(Plateau p,char current_player, char other_player){
     int score=-INFTY;
     for(int i=0;i<lengthOfMoves;i++){
         char* charac=moves[i];
-        m=translateMove(charac);
+        translateMove(m,charac);
         if (p[m[0][1]][m[1][1]]!=BLACK) {
         allMove(p,m,BLACK,WHITE);
         int newScore=miniMax(p,2,-INFTY,INFTY,false);
-        Move mback=createMove(m[0][1],m[1][1],m[0][0],m[1][0]);
+        mback[0][0]=m[0][1];
+        mback[1][0]=m[1][1];
+        mback[0][1]=m[0][0];
+        mback[1][1]=m[1][0];
         allMove(p,mback,BLACK,WHITE);
             if (newScore>score) {
             score=newScore;
-            bestMove=m;
+            bestMove[0][0]=m[0][0];
+            bestMove[1][0]=m[1][0];
+            bestMove[0][1]=m[0][1];
+            bestMove[1][1]=m[1][1];
             }
     }
     }
