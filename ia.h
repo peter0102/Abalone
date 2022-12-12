1577f3606e2ca62bcfea0cd45b9189f3a4a670bc
peter.lin@uha.fr <peter.lin@uha.fr@6219d9c7-8401-0010-ae78-9bc666ba87c6>
2022-12-12 13:31:18 +0000

    Ajout de ia.c et ia.h contenant l'algorithme miniMax et les fonctions de déplacement de l'IA et du joueur

---
+++ ia.h
@@ -0,0 +1,24 @@
+#include "global.h"
+/**
+ * \fn minimax(Plateau p, int depth, int alpha, int beta, bool isMaximizingPlayer)
+ * \brief Algorithme MiniMax, qui retourne le score du meilleur mouvement, l'IA maximise son utilité, le joueur minimise l'utilité de l'IA
+ * \param p Etat du plateau de jeu
+ * \param depth Profondeur voulue de l'arbre
+ * \param alpha plus grande valeur pour MAX parmi les successeurs
+ * \param beta plus petite valeur pour MIN parmi les successeurs
+ * \param isMaximizingPlayer booléen true si on cherche à maximiser l'utilité, false sinon
+ * \return Retourne le score le plus haut correspondant au "meilleur" mouvemement
+*/
+int miniMax(Plateau p,int depth,int alpha,int beta,bool isMaximizingPlayer);
+/**
+ * \fn aiMove(Plateau p)
+ * \brief L'IA effectue un mouvement
+ * \param p Etat du plateau de jeu
+*/
+char aiMove(Plateau p);
+/**
+ * \fn playerMove(Plateau p)
+ * \brief Demande à l'utilisateur d'entrer des coordonnées et effectue le mouvement qui correspond
+ * \param p Etat du plateau de jeu
+*/
+char playerMove(Plateau p);
