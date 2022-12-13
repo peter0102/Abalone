675044ecd81bb8b777848f2cb433803f4f5ae9fd
peter.lin <peter.lin@uha.fr>
2022-12-13 18:43:55 +0000

    Correction des fonctions de evaluate, suppression des include inutiles

--- ListeDeplacementsString.c
+++ ListeDeplacementsString.c
@@ -2,39 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "global.h"
 
-#define MAX_I 10
-#define MAX_J 10
-#define INFTY 147483648
 #define nombrePions 14
 #define tailleListeCoups 80
-
-#define EMPTY '0'
-#define BLACK 'B'
-#define WHITE 'W'
-
-//types de mouvement
-#define LINE_HOR_RIGHT 'a'
-#define LINE_HOR_LEFT 'b'
-#define LINE_VER_DOWN 'c'
-#define LINE_VER_UP 'd'
-
-#define LAT_2_HOR 'e'
-#define LAT_2_VER 'f'
-
-#define LAT_3_VER_X0_DOWN 'g'
-#define LAT_3_VER_X0_UP 'h'
-#define LAT_3_HOR_Y0_RIGHT 'i'
-#define LAT_3_HOR_Y0_LEFT 'j'
-
-//gestion d'erreurs
-#define ERROR 'x'
-#define SUCCESS 'l'
-
-//définition des types
-typedef char Board[10][10]; //caractères ASCII
-typedef char Move[2][2]; //Mouvement {{x0,x1},{y0,y1}} valeurs numériques
-typedef char Triple[2][3]; //coordonnées d'un joueur {{xj0,xj1,xj2},{yj0,yj1,yj2}} valeurs numériques
 
 char otherPlayer(char player);
 
