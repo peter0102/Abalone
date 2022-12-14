24ebc0de92919fdb0237ba98211dce73371474f8
mahmoud.khoubji <mahmoud.khoubji@uha.fr>
2022-12-14 09:26:53 +0000

    Création du serveur et du client

---
+++ server.c
@@ -0,0 +1,24 @@
+#include "server.h"
+
+SOCKET TCPCreateServer(short service){
+    struct protoent *ppe = getprotobyname("tcp");
+    if (ppe==NULL) exit(1);
+
+    SOCKET s = socket (AF_INET,SOCK_STREAM,ppe->p_proto);
+    if (s==INVALID_SOCKET) exit(1);
+
+    struct sockaddr_in sin;
+    memset (&sin,0,sizeof(sin));
+    sin.sin_family=AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(service);
+
+    int opt = 1;
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &opt,1 );
+
+    if(bind(s,(struct sockaddr*)&sin,sizeof(sin))==SOCKET_ERROR) exit(1);
+
+    if(listen(s,5) == SOCKET_ERROR) exit(1);
+    return s;
+}
+
--- server.h
+++ server.h
@@ -1,8 +1,10 @@
+#ifndef ABALONE_SERVER
+#define ABALONE_SERVER
 
-
-#ifndef ABALONE_NETWORK
-#define ABALONE_NETWORK
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>  
 #include <sys/types.h>
 #include <netdb.h>
@@ -16,16 +18,10 @@ typedef struct sockaddr SOCKADDR;
 typedef struct in_addr IN_ADDR;
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 
-
-SOCKET TCP_Create_Client(char* severName,short service);
 SOCKET TCP_Create_Server(short service);
 
 
 
-#endif //ABALONE_NETWORK
+#endif //ABALONE_SERVER
