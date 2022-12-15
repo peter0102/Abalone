1d8b0ae1f9c10bd40c73cb95e7359260f82af23a
mahmoud.khoubji <mahmoud.khoubji@uha.fr>
2022-12-15 20:44:36 +0000

    Headers du sserver.h et client.h

--- src/server.h
+++ src/server.h
@@ -1,3 +1,10 @@
+/**
+ * \file server.h
+ * \brief Création d'un serveur 
+ * \author Groupe 1
+ * \version 0.1
+*/
+
 #ifndef ABALONE_SERVER
 #define ABALONE_SERVER
 
@@ -19,7 +26,12 @@ typedef struct in_addr IN_ADDR;
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
 
-
+/**
+ * \fn TCPCreateServer(short service)
+ * \brief Création d'un serveur
+ * \param service Le numéro de port
+ * \return Retourne le descripteur de la socket
+*/
 SOCKET TCPCreateServer(short service);
 
 
